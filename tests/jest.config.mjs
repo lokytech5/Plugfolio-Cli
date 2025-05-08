516b0d5ac01267a9e3d673364fbdc35fb43f2163
lokytech5 <lokytech5@gmail.com>
2025-05-08 14:47:04 +0100

    change test config file extension to mjs instead of ts, and updated script file

---
+++ tests/jest.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
